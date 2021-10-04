7dac7ba7ec9f796b92ecaa555faf62282a046b7d
afzal mahmud <afzzalmahmud@gmail.com>
2021-10-04 23:17:20 +0600

    forget to add price on services

--- src/Pages/TopServices/TopServices.js
+++ src/Pages/TopServices/TopServices.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 function TopServices(props) {
-    const { image, description, service } = props.serviceObject;
+    const { image, description, service, price } = props.serviceObject;
 
     return (
             <Card className="text-center m-4 p-4">
                 <div className='img-container'>
                     <img src={image} alt="" />
+                    <h4 className='pt-4'>Price : {price} </h4>
                 </div>
                 <Card.Body>
-                    <Card.Title className='p-4'>{service}</Card.Title>
+                    <Card.Title className='p-3 fw-bold'>{service}</Card.Title>
                     <Card.Text>
                         {description}
                     </Card.Text>
